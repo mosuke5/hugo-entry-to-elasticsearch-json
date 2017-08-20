133889a3fa571b84e641b6cbcf7c44b001cdec6c
mosuke5 <w.vamos603@gmail.com>
2017-08-20 06:37:51 +0000

    update

--- main.rb
+++ main.rb
@@ -4,15 +4,32 @@ require "tomlrb"
 require "commonmarker"
 require "sanitize"
 
+# path of hugo markdown contents
 contents_path = ARGV[0]
+
 Dir.glob("#{contents_path}**/*.md") do |entry_list|
   File.open(entry_list, "r") do |entry| 
-     entry_raw_data = entry.read.split("+++\n")
-     entry_data = Tomlrb.parse(entry_raw_data[1])
-     body_html = CommonMarker.render_html(entry_raw_data[2])
-     body = Sanitize.fragment(body_html)
-     entry_data["body"] =  body
-     puts "{ \"index\" : {} }"
-     puts entry_data.to_json
+    # Split data to meta info and body
+    entry_raw_data = entry.read.split("+++\n")
+
+    # Convert meta info(toml) to ruby obj
+    entry_data = Tomlrb.parse(entry_raw_data[1])
+
+    # temporary hash for output
+    tmp = {}
+    
+    # convert hash key name to letter(ex. Tags -> tags)
+    entry_data.each do |k, v|
+      tmp[k.downcase] = v
+    end
+    
+    # sanitize html tags
+    body_html = CommonMarker.render_html(entry_raw_data[2])
+    body = Sanitize.fragment(body_html)
+    tmp["body"] =  body
+
+    # output 
+    puts "{ \"index\" : {} }"
+    puts tmp.to_json
   end
 end
